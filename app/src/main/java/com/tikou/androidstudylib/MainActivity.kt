e0a65012bb012fd8ef43c74b299797fc79d0fb5f
ww <1320917731@qq.com>
2022-06-13 16:22:45 +0800

    ww:AndroidArchitect    初始化

---
+++ app/src/main/java/com/tikou/androidstudylib/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tikou.androidstudylib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
